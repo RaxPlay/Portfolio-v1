7437fdc2649bf579d48dabafdc136a793e58a402
RaxPlay <121737678+RaxPlay@users.noreply.github.com>
2025-02-15 11:49:39 -0800

    Scrolling Functions added the scrolling functions with its respective

---
+++ app.js
@@ -0,0 +1,32 @@
+//Scrolling 
+function scrollToHome(){
+    window.scrollTo({
+        top: 0, 
+
+        behavior: "smooth" 
+    })
+}
+
+function scrollToTechnologies(){
+    window.scrollTo({
+        top: 200,
+
+        behavior: "smooth"
+    })
+}
+
+function scrollToPortfolio(){
+    window.scrollTo({
+        top: 720,
+
+        behavior: "smooth"
+    })
+}
+
+function scrollToContact (){
+    window.scrollTo({
+        top: 1250,
+
+        behavior: "smooth"
+    })
+}
